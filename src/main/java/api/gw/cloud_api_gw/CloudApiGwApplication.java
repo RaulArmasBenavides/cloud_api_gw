9ca63d024d33f524df597baa1ecafc39fbc9f3c1
Raúl Armas <raularmasbx@gmail.com>
2024-06-18 00:28:31 -0500

    RMAB first commit

---
+++ src/main/java/api/gw/cloud_api_gw/CloudApiGwApplication.java
@@ -0,0 +1,14 @@
+package api.gw.cloud_api_gw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@SpringBootApplication
+// @EnableEurekaClient
+public class CloudApiGwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudApiGwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/gw/cloud_api_gw/CloudApiGwApplicationTests.java
@@ -0,0 +1,13 @@
+package api.gw.cloud_api_gw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApiGwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
