c4d4cb316ece443f3fec789b86e93064d87d1333
Raúl Armas <raularmasbx@gmail.com>
2025-08-29 18:22:27 -0500

    Updates docker

--- src/main/java/api/gw/cloud_api_gw/CloudApiGwApplication.java
+++ src/main/java/api/gw/cloud_api_gw/CloudApiGwApplication.java
@@ -3,7 +3,9 @@ package api.gw.cloud_api_gw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
+@PropertySource("classpath:env.properties")
 @EnableDiscoveryClient
 public class CloudApiGwApplication {
 
